fd61d201bfe415fc9fa48583290db7945df8a56d
Isyara Capstone Project Bangkit Batch 2 2024 <isyara.bangkit@gmail.com>
2024-12-09 11:15:25 +0700

    Update Theme.kt

--- app/src/main/java/com/application/isyara/theme/Theme.kt
+++ app/src/main/java/com/application/isyara/theme/Theme.kt
@@ -10,48 +10,41 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.application.isyara.ui.theme.DarkBackground
+import com.application.isyara.ui.theme.DarkOnPrimary
+import com.application.isyara.ui.theme.DarkPrimary
+import com.application.isyara.ui.theme.DarkSecondary
+import com.application.isyara.ui.theme.DarkTextPrimary
+import com.application.isyara.ui.theme.LightBackground
+import com.application.isyara.ui.theme.LightOnPrimary
+import com.application.isyara.ui.theme.LightPrimary
+import com.application.isyara.ui.theme.LightSecondary
+import com.application.isyara.ui.theme.LightTextPrimary
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Blue80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+// Light Theme Color Scheme
+private val LightColorPalette = lightColorScheme(
+    primary = LightPrimary,
+    secondary = LightSecondary,
+    surface = LightBackground,
+    onPrimary = LightOnPrimary,
+    onSecondary = LightTextPrimary,
+    onSurface = LightTextPrimary
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Blue80,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+// Dark Theme Color Scheme
+private val DarkColorPalette = darkColorScheme(
+    primary = DarkPrimary,
+    secondary = DarkSecondary,
+    surface = DarkBackground,
+    onPrimary = DarkOnPrimary,
+    onSecondary = DarkTextPrimary,
+    onSurface = DarkTextPrimary
 )
-
 @Composable
-fun IsyaraTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
+fun IsyaraTheme(darkTheme: Boolean = false, content: @Composable () -> Unit) {
+    val colors = if (darkTheme) DarkColorPalette else LightColorPalette
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = colors,
         typography = Typography,
         content = content
     )
